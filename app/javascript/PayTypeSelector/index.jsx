aa8e5b3c63fa34b780ca1268ed68ae18ef1c344a
JimenezVero <iruzzz20@gmail.com>
2017-07-11 01:29:15 -0500

    Carrito de compras

---
+++ app/javascript/PayTypeSelector/index.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+import NoPayType            from './NoPayType';
+import CreditCardPayType    from './CreditCardPayType';
+import CheckPayType         from './CheckPayType';
+import PurchaseOrderPayType from './PurchaseOrderPayType';
+
+class PayTypeSelector extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onPayTypeSelected = this.onPayTypeSelected.bind(this);
+    this.state = { selectedPayType: null };
+  }
+
+  onPayTypeSelected(event) {
+    this.setState({ selectedPayType: event.target.value });
+  }
+
+  render() {
+    let PayTypeCustomComponent = NoPayType;
+    if (this.state.selectedPayType == "Credit card") {
+      PayTypeCustomComponent = CreditCardPayType;
+    } else if (this.state.selectedPayType == "Check") {
+      PayTypeCustomComponent = CheckPayType;
+    } else if (this.state.selectedPayType == "Purchase order") {
+      PayTypeCustomComponent = PurchaseOrderPayType;
+    }
+    return (
+      
+          </select>
+        </div>
+        <PayTypeCustomComponent />
+      </div>
+    );
+  }
+}
+export default PayTypeSelector
